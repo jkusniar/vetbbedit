7641c88e40a5820398942101abf21a9d55c1abfd
Jan Kusniar <jkusniar@gmail.com>
2017-05-19 15:17:23 +0200

    News GUI WIP

--- http/client/app.js
+++ http/client/app.js
@@ -5,11 +5,37 @@ Vue.use(VueMaterial);
 new Vue({
     el: '#app',
     data: {
-        greeting: 'Hellou!'
+        news: [],
+        newNews: ""
+    },
+    mounted: function () {
+        this.getNews();
     },
     methods: {
-        toggleSideNav: function() {
+        toggleSideNav: function () {
             this.$refs.sideNav.toggle();
+        },
+        getNews: function () {
+            this.news = [];
+            var vm = this;
+            axios.get('/news')
+                .then(function (response) {
+                    console.log(response.data);
+                    console.log(response.status);
+                    console.log(response.statusText);
+                    vm.news = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        onNewNews: function (event) {
+            console.log(event.key);
+            if(event.key === "Enter") {
+                console.log('the id of the input was:' + event.currentTarget.id);
+                console.log(this.newNews);
+            }
+
         }
     }
 });
