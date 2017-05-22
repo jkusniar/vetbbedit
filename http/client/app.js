10b958b7a66d7401d87880ff96e8665fd58334fc
Jan Kusniar <jkusniar@gmail.com>
2017-05-22 15:02:06 +0200

    news GUI WIP2

--- http/client/app.js
+++ http/client/app.js
@@ -6,7 +6,8 @@ new Vue({
     el: '#app',
     data: {
         news: [],
-        newNews: ""
+        newNews: "",
+        respMsg: ""
     },
     mounted: function () {
         this.getNews();
@@ -20,22 +21,50 @@ new Vue({
             var vm = this;
             axios.get('/news')
                 .then(function (response) {
-                    console.log(response.data);
-                    console.log(response.status);
-                    console.log(response.statusText);
-                    vm.news = response.data;
+                    vm.news = response.data.items;
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
         },
         onNewNews: function (event) {
-            console.log(event.key);
-            if(event.key === "Enter") {
-                console.log('the id of the input was:' + event.currentTarget.id);
-                console.log(this.newNews);
+            if (this.newNews) {
+                this.news.push(this.newNews);
+                this.newNews = "";
             }
+        },
+        removeItem: function (index) {
+            //console.log("deleting " + index)
+            this.news.splice(index, 1);
+        },
+        saveAndUpload: function () {
+            this.respMsg = "";
+            var vm = this;
+            axios.put('/news', {items: this.news})
+                .then(function (response) {
+                    vm.respMsg = "save OK";
+                    vm.showMessage()
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                        vm.respMsg = error.response.data;
+                    } else if (error.request) {
+                        console.log(error.request);
+                        vm.respMsg = "no response";
+                    } else {
+                        console.log('Error', error.message);
+                        vm.respMsg = error.message;
+                    }
+                    console.log(error.config);
 
+                    vm.showMessage()
+                });
+        },
+        showMessage: function () {
+            this.$refs.snackbar.open();
         }
     }
 });
